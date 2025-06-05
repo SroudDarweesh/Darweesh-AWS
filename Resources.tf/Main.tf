4162328d1ce560ff671a62838be304dd08832d70
unknown <Administrator@DARWEESH.S>
2025-06-04 21:27:13 -0700

    Add VPC and subnet resources

---
+++ Resources.tf/Main.tf
@@ -0,0 +1,27 @@
+resource "aws_vpc" "name" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "Darweesh-VPC"
+  }
+}
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.name.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Darweesh-Public-Subnet"
+  }
+}
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.name.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "Darweesh-Private-Subnet"
+  }
+}
